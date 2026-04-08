bca335aeecc87d5b92cd20d6c4c6a567dbf56c80
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-08 22:18:22 +0530

    display crypto name, symbol, and price in card

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -1,6 +1,16 @@
 export const CryptoCard = ({crypto}) => {
-    return <div className="bg-gray-700 text-white p-4 rounded-xl flex items-center gap-4 hover:bg-gray transition">
+    return <div className="bg-black border border-gray-800 text-white p-4 rounded-xl flex items-center gap-4 hover:bg-gray-400 transition">
         <img src={crypto.image} alt={crypto.name} 
         className="w-10 h-10"/>
+
+        <div>
+            <h2 className="font-semibold">{crypto.name}</h2>
+            <p className="text-gray-400 text-sm uppercase">{crypto.symbol}</p>
+        </div>
+
+        <div classname="ml-auto text-right">
+            <p className = "font-semibold">${crypto.current_price}</p>
+        </div>
     </div>
+
 }
