c9a53421f9dde1f397ccb593599d8a2f52cb6a52
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-09 10:49:14 +0530

    Improve UI and bugs

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -2,33 +2,39 @@ import { formatPrice } from "../utils/formatter"
 
 export const CryptoCard = ({crypto}) => {
     return (
-        <div className="bg-black border border-gray-800 text-white p-4 rounded-xl flex items-center justify-between hover:bg-gray-900 transition">
+        <div className="bg-black border border-gray-800 text-white p-4 rounded-xl flex flex-col hover:bg-gray-900 transition">
 
             <div className="flex items-center gap-4">
-                <img src={crypto.image} alt={crypto.name}
+               <div>
+                 <img src={crypto.image} alt={crypto.name}
                 className="w-10 h-10"/>
+               </div>
 
                 <div>
                     <h2 className="font-semibold">{crypto.name}</h2>
                     <p className="text-gray-400 text-sm uppercase">{crypto.symbol}</p>
 
                     <span className="rounded-xl bg-purple-500 px-2 text-black text-sm">#{crypto.market_cap_rank}</span>
-
-                <p className="font-semibold">
-                    {formatPrice(crypto.current_price)}
-                </p>
-
-                <p className={`text-sm ${
-                    crypto.price_change_percentage_24h >= 0
-                    ? "text-green-400"
-                    :"text-red-400"
-                }`}>
-                    {crypto.price_change_percentage_24h.toFixed(2)}%
-                </p>
                 </div>
             </div>
 
-
+              <div className="mt-2 ml-2">
+                 <p className="font-semibold text-xl">
+                    {formatPrice(crypto.current_price)}
+                </p>
+                  <div className="flex mt-2"> 
+                    <p className={` text-sm rounded-md font-medium p-2 px-2 py-1 ${
+                    crypto.price_change_percentage_24h >= 0
+                    ? " bg-green-400/20 text-green-400"
+                    : " bg-red-400/20 text-red-400"
+                }`}>
+                    {crypto.price_change_percentage_24h >= 0
+                    ? "↑ "
+                    :"↓ "}
+                    {crypto.price_change_percentage_24h.toFixed(2)}
+                </p>
+                  </div>
+              </div>
         </div>
     )
 
