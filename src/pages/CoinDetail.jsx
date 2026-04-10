265f9a8569e5c43a626a5f3d22a8511b445a42e4
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-10 21:20:50 +0530

    Add API call for coin details

--- src/pages/CoinDetail.jsx
+++ src/pages/CoinDetail.jsx
@@ -1,5 +1,27 @@
+import { useParams } from "react-router"
+import { fetchCoinData } from "../api/coinGecko";
+
 export const CoinDetail = () => {
+
+    const {id} = useParams();
+    const [coin, setCoin] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    
+
+    const loadCoinData = async () => {
+          try{
+                    const data = await fetchCoinData();
+                setCoin(data);
+                console.log(data);
+                }catch (err) {
+                    console.error("Error fetching cryptos:",err);
+                }finally {
+                    setIsLoading(false);
+                }
+    }
     return <div>
-        Coin page
+        This is the coin page: {id}
+        
     </div>
 }
