ab0e3749c2cefb3f119ecfb034b84af25244801f
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-10 23:10:36 +0530

    Add loading state for coin detail page

--- src/pages/CoinDetail.jsx
+++ src/pages/CoinDetail.jsx
@@ -25,13 +25,27 @@ export const CoinDetail = () => {
                     setIsLoading(false);
                 }
     };
-     if (!coin){
-        return(
-            <div className="min-h-screen bg-black justify-center items-center flex flex-col ">
-                <div className="justify-items-center flex flex-col border-2 border-red-900 px-30 py-20 rounded-2xl shadow-2xl shadow-red-900">
-                 <p className="text-3xl text-red-800 font-bold mb-5">Coin not Found</p>
+
+        if (isLoading){
+            return (
+                <div className="flex flex-col items-center justify-center p-16 gap-4 text-[#e0e0e0]">
+        
+             <div className="w-[50px] h-[50px] border-4 border-[rgba(173,216,230,0.2)] border-t-purple-500 rounded-full animate-spin"></div>
+        
+             <p>Loading coin data...</p>
+
+             </div>
+            )
+        }
+
+
+         if (!coin){
+            return(
+            <div className="min-h-screen bg-black justify-center items-center flex ">
+                <div className="justify-items-center flex-col flex border-2 border-red-900 px-30 py-20 rounded-2xl shadow-2xl shadow-red-900">
+                 <p className="text-3xl text-red-800 font-bold mb-5">COIN NOT FOUND</p>
                 <button onClick={()=>navigate("/")}
-                className="border-2 border-gray-800 py-2 rounded-md  text-red-800 mt-5 font-semibold">Go Back</button>
+                className="border-2 border-red-900 py-2 rounded-md  text-red-800 mt-5 font-semibold">Go Back</button>
                 </div>
             </div>
         )
